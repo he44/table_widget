1202df4f64ab6a8659f2c230c700ffc965564261
he44 <johnwatson221bbaker@outlook.com>
2023-11-18 17:13:10 -0800

    feat: add buttons to interact with the table and another input box

--- src/TableComponent.tsx
+++ src/TableComponent.tsx
@@ -22,7 +22,11 @@ const options: { value: string; label: string; type: KeyType }[] = [
 ];
 
 const TableComponent: React.FC = () => {
-    const [rows, setRows] = useState<TableRow[]>([]);
+    const [rows, setRows] = useState<TableRow[]>([
+        { key: '--int-flag-1', keyType: 'integer', value: 0 },
+        { key: '--bool-flag-1', keyType: 'bool', value: false },
+        { key: '--string-flag-1', keyType: 'string', value: '' }
+    ]);
 
     const handleKeyChange = (
         selectedOption: SingleValue<{ value: string; label: string; type: KeyType }>,
@@ -52,54 +56,83 @@ const TableComponent: React.FC = () => {
         setRows([...rows, newRow]);
     };
 
+    const deleteRow = (index: number) => {
+        const newRows = rows.filter((_, rowIndex) => rowIndex !== index);
+        setRows(newRows);
+    };
+
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Key</th>
-                    <th>Value</th>
-                </tr>
-            </thead>
-            <tbody>
-                {rows.map((row, index) => (
-                    <tr key={index}>
-                        <td>
-                            <Select
-                                defaultValue={{ value: row.key, label: row.key, type: row.keyType }}
-                                options={options}
-                                onChange={(option) => handleKeyChange(option, index)}
-                            />
-                        </td>
-                        <td>
-                            {row.keyType === 'bool' ? (
-                                <input
-                                    type="checkbox"
-                                    checked={row.value}
-                                    onChange={(e) => handleValueChange(e, index)}
-                                />
-                            ) : row.keyType === 'integer' ? (
-                                <input
-                                    type="number"
-                                    value={row.value}
-                                    onChange={(e) => handleValueChange(e, index)}
-                                />
-                            ) : (
-                                <input
-                                    type="text"
-                                    value={row.value}
-                                    onChange={(e) => handleValueChange(e, index)}
-                                />
-                            )}
-                        </td>
+        <div style={{ width: '90%', margin: 'auto' }}>
+            <table style={{ width: '100%', boxSizing: 'border-box' }}>
+                <thead>
+                    <tr>
+                        <th>Flag</th>
+                        <th colSpan={2}>Value</th>
                     </tr>
-                ))}
-                <tr>
-                    <td colSpan={2}>
-                        <button onClick={addNewRow} style={{ width: '100%' }}>Add Row</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {rows.map((row, index) => (
+                        <tr key={index}>
+                            <td>
+                                <Select
+                                    defaultValue={{ value: row.key, label: row.key, type: row.keyType }}
+                                    options={options}
+                                    onChange={(option) => handleKeyChange(option, index)}
+                                />
+                            </td>
+                            <td>
+                                {row.keyType === 'bool' ? (
+                                    <input
+                                        type="checkbox"
+                                        checked={row.value}
+                                        onChange={(e) => handleValueChange(e, index)}
+                                    />
+                                ) : row.keyType === 'integer' ? (
+                                    <input
+                                        type="number"
+                                        value={row.value}
+                                        onChange={(e) => handleValueChange(e, index)}
+                                    />
+                                ) : (
+                                    <input
+                                        type="text"
+                                        value={row.value}
+                                        onChange={(e) => handleValueChange(e, index)}
+                                    />
+                                )}
+                            </td>
+                            <td>
+                                <button onClick={() => deleteRow(index)} style={{ border: 'none', background: 'none' }}>
+                                    &#10006; {/* Represents a tiny cross */}</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table >
+            <div style={{ textAlign: 'right', marginTop: '10px' }}>
+                <button
+                    onClick={addNewRow}
+                    style={{
+                        width: '30%', // Shorter width
+                        padding: '5px', // Adjust padding for height
+                        textAlign: 'center',
+                        backgroundColor: 'rgba(138, 43, 226, 0.65)', // Foxglove purple with transparency
+                        color: 'white', // Text color
+                        fontWeight: 'bold', // Bold font
+                        border: 'none', // Optional: remove border
+                        borderRadius: '5px', // Optional: rounded corners
+                        cursor: 'pointer' // Changes cursor on hover
+                    }}
+                >
+                    Set a new flag
+                </button>
+            </div>
+            <input
+                type="text"
+                placeholder='Other flags you might want to pass. E.g. --other-flag="some value"'
+                style={{ width: '100%', marginTop: '20px', height: '15px', padding: '10px', boxSizing: 'border-box' }} // Add some styling as needed
+            />
+        </div >
     );
 };
 
